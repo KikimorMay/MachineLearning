d44002770a4b80d9277bd9be239fa4234c2059a7
aaa\xj <244218199@qq.com>
2017-07-05 22:31:57 +0800

    test

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -41,7 +41,7 @@ def autoNorm(dataset):
 #     group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
 #     labels = ['A', 'A', 'B', 'B']
 #     return group, labels
-
+######
 
 
     #  kNN分类
