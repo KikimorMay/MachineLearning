29a91aab825db10c3ea032af44e61a844f135043
aaa\xj <244218199@qq.com>
2017-08-07 11:43:55 +0800

    no message

--- cs231/assignment_1/knn.py
+++ cs231/assignment_1/knn.py
@@ -11,46 +11,59 @@ def get_CIFAR10_data(num_training = 49000, num_validation = 1000, num_test = 100
 
     mask = list(range(num_training, num_training  + num_validation))
     X_val = np.array(X_train[mask].reshape(num_validation, 32*32*3), dtype = 'float32')
-    y_val = np.array(y_train[mask], dtype = 'float32')
+    y_val = np.array(y_train[mask])
     mask = list(range(num_training))
     X_train = np.array(X_train[mask].reshape(num_training, 32*32*3), dtype = 'float32')
     y_train = np.array(y_train[mask])
     mask = list(range(num_test))
     X_test = np.array(X_test[mask].reshape(num_test, 32*32*3), dtype = 'float32')
-    y_test =  np.array(y_test[mask], dtype = 'float32')
+    y_test =  np.array(y_test[mask])
 
     return X_train, y_train, X_val, y_val, X_test, y_test
 
+def prediction(X_test, W, b):
+    y_test_pre = tf.nn.softmax(tf.matmul(X_test, W) + b)
+    return y_test_pre
+
+def evaluting(y_test_pre, y_test):
+    correct_prediction = tf.equal(tf.argmax(y_test_pre, 1), tf.argmax(y_test, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
 
 
-
-X_train = tf.placeholder(tf.float32, [None, 32*32*3])
-y_train = tf.placeholder(tf.float32, [None, 10])
-W = tf.Variable(tf.zeros([32*32*3, 10]))
-b = tf.Variable(tf.zeros([10]))
-
-multi_result = tf.matmul(X_train, W) + b
-pred_y = tf.nn.softmax(multi_result)
-
-loss = tf.reduce_mean(-tf.reduce_sum(y_train * tf.log(pred_y), reduction_indices=[1]))
-init = tf.global_variables_initializer()
-
-train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
-X_train_, y_train_, X_val_, y_val_, X_test_, y_test_ = get_CIFAR10_data()
-
-
-with tf.Session() as sess:
-    sess.run(init)
-    for i in range(5000):
-        batch_ys = np.zeros([100, 10])   #每个batch 是100个样本
-        j = i%490
-        batch_xs = X_train_[j*100:(j+1)*100, :]
-        batch_ys_y = y_train_[j*100:(j+1)*100]
-        batch_ys[list(range(100)), batch_ys_y] = 1
-        sess.run(train_step, feed_dict={X_train: batch_xs, y_train: batch_ys})
-
-        if i % 50 == 0:
-            print(sess.run(multi_result, feed_dict={X_train: batch_xs, y_train: batch_ys}))
-
-
-
+# X_train = tf.placeholder(tf.float32, [None, 32*32*3])
+# y_train = tf.placeholder(tf.float32, [None, 10])
+# W = tf.Variable(tf.zeros([32*32*3, 10]))
+# b = tf.Variable(tf.zeros([10]))
+#
+# multi_result = tf.matmul(X_train, W) + b
+# pred_y = tf.nn.softmax(multi_result)
+#
+# loss = tf.reduce_mean(-tf.reduce_sum(y_train * tf.log(pred_y), reduction_indices=[1]))
+# init = tf.global_variables_initializer()
+#
+# train_step = tf.train.GradientDescentOptimizer(0.0000001).minimize(loss)
+#
+# X_train_, y_train_, X_val_, y_val_, X_test_, y_test_ = get_CIFAR10_data()
+#
+#
+# with tf.Session() as sess:
+#     sess.run(init)
+#     for i in range(5000):
+#         batch_ys = np.zeros([1000, 10])   #每个batch 是100个样本
+#         j = i%49
+#         batch_xs = X_train_[j*1000:(j+1)*1000, :]
+#         batch_ys_y = y_train_[j*1000:(j+1)*1000]
+#         batch_ys[list(range(1000)), batch_ys_y] = 1
+#         _, loss_v = sess.run([train_step, loss], feed_dict={X_train: batch_xs, y_train: batch_ys})
+#         print('step %d loss=%f' % (i, loss_v))
+# #        if i % 50 == 0:
+# #            print(sess.run(multi_result, feed_dict={X_train: batch_xs, y_train: batch_ys}))
+#         if i == 4999:
+#             W_, b_ = sess.run([W, b], feed_dict={X_train: batch_xs, y_train: batch_ys})
+#             y_test_pre = prediction(X_test_, W_, b_)
+#             y_test__ = np.zeros([1000, 10])
+#             y_test__[list(range(1000)), y_test_] = 1
+#             accuracy = evaluting(y_test_pre, y_test__)
+#             print(sess.run(accuracy))
+#
--- cs231/assignment_1/two_layer_network.py
+++ cs231/assignment_1/two_layer_network.py
@@ -11,6 +11,11 @@ X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
 # X_val, X_test.shape (1000, 3072
 # y_val, y_test,shape (1000,
 
+def change_y_to_matrix(y_vector):
+    num = y_vector.shape[0]
+    y_matrix = np.zeros([num, 10])
+    y_matrix[list(range(num)), y_vector] = 1
+    return y_matrix
 
 def layer(inputs, insize, outsize, activation_function = None):
     W = tf.Variable(tf.random_normal([insize, outsize]))
@@ -28,34 +33,55 @@ def predition(y_softmax_pre , y_test):
         tf.cast(tf.equal(tf.argmax(y_softmax_pre, 1), tf.argmax(y_test, 1)), tf.float32)
     )
     return accuracy
-
+y_train_ = change_y_to_matrix(y_train)
+y_test_ = change_y_to_matrix(y_test)
 
 X_batch = tf.placeholder(tf.float32, [None, 3072])
 Y_batch = tf.placeholder(tf.float32, [None, 10])
 
-output1 = layer(X_batch, 3072, 100, tf.nn.tanh)
-output = layer(output1, 100, 10)
+output1 = layer(X_batch, 3072, 200, tf.nn.tanh)
+output = layer(output1, 200, 10)
+
 
 y_softmax_pre = tf.nn.softmax(output)
 loss = tf.reduce_mean(tf.reduce_sum(-Y_batch * tf.log(y_softmax_pre), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+accuracy = predition(y_softmax_pre, Y_batch)
+train_step = tf.train.GradientDescentOptimizer(0.005).minimize(loss)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    batch_size = 1000
-    step_size = 1000
-    for i in range(step_size):
-        j = i%(49000//batch_size)
-        X_batch_ = X_train[j*batch_size: (j+1)*batch_size, :]
-        Y_batch_y = y_train[j*batch_size: (j+1)*batch_size]
-        Y_batch_ = np.zeros([batch_size, 10])
-        Y_batch_[list(range(batch_size)), Y_batch_y] = 1
-        if i%50 == 0:
-            accuracy = predition(y_softmax_pre , Y_batch_)
-            loss_, acc = sess.run([loss, accuracy], feed_dict={X_batch: X_batch_, Y_batch:Y_batch_})
-            print('%d: loss:%f, acc = %f' %(i, loss_, acc))
-        sess.run(train_step, feed_dict={X_batch: X_batch_, Y_batch:Y_batch_})
+    batch_size = 100
+    epoch = 100
+    loss_train = []; acc_train = []; loss_test = []; acc_test = []
+    for i in range(epoch):
 
+        for j in range(X_train.shape[0]//batch_size):
+            X_batch_ = X_train[j*batch_size: (j+1)*batch_size, :]
+            Y_batch_ = y_train_[j*batch_size: (j+1)*batch_size, :]
+            sess.run(train_step, feed_dict={X_batch: X_batch_, Y_batch:Y_batch_})
+        loss_, acc = sess.run([loss, accuracy], feed_dict={X_batch: X_train, Y_batch: y_train_})
+        loss_2, acc_2 = sess.run([loss, accuracy], feed_dict={X_batch: X_test, Y_batch: y_test_})
 
+        loss_train.append(loss_); acc_train.append(acc)
+        loss_test.append(loss_2); acc_test.append(acc_2)
+
+        print('the training set:%d: loss:%f, acc = %f' % (i, loss_, acc))
+        print('the test set:%d: loss:%f, acc:%f\n' %(i, loss_2, acc_2))
+np.save('loss_train.npy', loss_train)
+np.save('acc_train.npy', acc_train)
+np.save('loss_test.npy', loss_test)
+np.save('acc_test.npy', acc_test)
+
+x = list(range(100))
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1)
+ax1.plot(x,loss_train,'b-',label='loss_train') # in 'bo-', b is blue, o is O marker, - is solid line and so on
+ax1.plot(x,loss_test,'y-',label='loss_test')
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.plot(x,acc_train,'g-',label='acc_train')
+ax2.plot(x,acc_test,'c-',label='acc_test')
+
+plt.show()
